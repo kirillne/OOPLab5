d5424e4bac3073beb0d19fb75e068efef787f6fc
kirillne <kirillne35@gmail.com>
2014-12-12 17:26:19 +0300

    XMLNode

--- MyXml/XMLNode.cpp
+++ MyXml/XMLNode.cpp
@@ -1,14 +1,6 @@
 #include "stdafx.h"
 #include "XMLNode.h"
-
-XMLNode::XMLNode()
-{
-}
-
-
-XMLNode::~XMLNode()
-{
-}
+#include "Utils.h"
 
 void XMLNode::Initialize(Object* owner)
 {
@@ -19,3 +11,18 @@ void XMLNode::Finalize()
 {
 	Object::Finalize(); // inherited
 }
+
+XMLNode* XMLNode::Parent()
+{
+	XMLNode* parent = as<XMLNode*>(Owner());
+	if (parent != NULL)
+		return parent;
+	else
+		return NULL;
+}
+
+void XMLNode::SetParent(XMLNode* value)
+{
+	SetOwner(value);
+}
+
--- MyXml/XMLNode.h
+++ MyXml/XMLNode.h
@@ -2,32 +2,20 @@
 #include "Config.h"
 
 #include <string>
-#include "../System/Object.h"
+#include "Object.h"
 #include "ValueArray.h"
 
 
 class XMLNode : public Sys::Object
 {
 public:
-	XMLNode();
-	~XMLNode();
-	virtual const char* Name();
-	virtual void SetName(const char* value);
+	virtual std::string ToStirng() = 0;
 	XMLNode* Parent();
 	void SetParent(XMLNode* value);
 
-	int ChildCount();
-	XMLNode* Child(int index);
-	XMLNode* FindChild(const char* path, const char* itemName, const char* itemValue); // names in path are delimited by '/'
-	const char* ChildItemValueByName(const char* path, const char* itemName, const char* defaultValue);
-	XMLNode* CreateChild(const char* name);
 protected:
 	virtual void Initialize(Object* owner); // override;
 	virtual void Finalize(); //override;
-
-private:
-	std::string m_Name;
-	Collection* m_Children;
 };
 
 
